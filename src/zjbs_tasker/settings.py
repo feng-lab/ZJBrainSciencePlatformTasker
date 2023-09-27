73c6d8eb96829c85043bda981657342ef32c8026
蔡涛 <caitao@zhejianglab.com>
2023-09-27 11:10:38 +0800

    添加rq配置

--- src/zjbs_tasker/settings.py
+++ src/zjbs_tasker/settings.py
@@ -8,10 +8,14 @@ class Settings(BaseSettings):
     DEBUG_MODE: bool = False
 
     # 日志目录
-    LOG_DIR: Path = Path(__file__).parent.parent.parent / "debug_data"
+    LOG_DIR: Path = Path(__file__).parent.parent.parent / "debug_data" / "log" / "tasker"
 
     # 数据库URL
     DATABASE_URL: str = "postgresql://zjlab:zjlab2023@localhost:7100/zjbs-tasker"
+    # 文件服务器URL
+    FILE_SERVICE_URL: str = "http://localhost:7200"
+    # Redis服务IP和端口
+    REDIS_HOST_PORT: str = "localhost:7300"
 
 
 settings: Settings = Settings()
